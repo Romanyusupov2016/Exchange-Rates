84497eca3478c75426b0dc000a994602217a4c4b
Roman Yusupov <romyusupov2015@gmail.com>
2020-01-28 12:39:57 +0500

    Init commit

---
+++ js/app.js
@@ -0,0 +1,45 @@
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        isShown: false,
+        errorInfo: '',
+        jsObj: {
+            date: '',
+            timeStamp: ''
+        },
+        valutes: [],
+        selectedValutes: []
+    },
+
+    methods: {
+        makeRequest() {
+        const url = 'https://www.cbr-xml-daily.ru/daily_json.js';
+            
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                this.jsObj.date = data.Date;
+                this.jsObj.timeStamp = data.Timestamp;
+                this.valutes = Object.entries(data.Valute);
+                for (let arr of this.valutes) {
+                    switch(arr[0]) {
+                        case 'USD':
+                            this.selectedValutes.push(arr[1]);
+                            break;
+                        case 'EUR':
+                            this.selectedValutes.push(arr[1]);
+                    }
+                }
+            })
+            .catch(err => {
+                this.errorInfo = err;
+                this.isShown = true;
+            })
+        },   
+    }
+})
+
+app.makeRequest();
+
+
